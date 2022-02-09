11cc19213ca7307fbc7c6059d587af5028a75e3e
Elias Bruhn <elias@lfpost.dk>
2022-02-09 16:03:22 +0100

    Add button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import "./App.css"
-import { Vocab } from "./vocabList"
+import { useState } from 'react';
+import './App.css';
+import { Vocab } from './vocabList';
 
 const App = () => {
-	const randomNum = Math.floor(Math.random() * Math.floor(Vocab.length))
-	return (
-		<>
-			<h1>{Vocab[randomNum].definition}</h1>
-			<h3>{Vocab[randomNum].word}</h3>
-		</>
-	)
-}
+  const [randomNum, setRandomNum] = useState(
+    Math.floor(Math.random() * Math.floor(Vocab.length))
+  );
+  return (
+    <>
+      <h1>{Vocab[randomNum].definition}</h1>
+      <h3>{Vocab[randomNum].word}</h3>
+      <button
+        onClick={() =>
+          setRandomNum(Math.floor(Math.random() * Math.floor(Vocab.length)))
+        }
+      >
+        Get new word
+      </button>
+    </>
+  );
+};
 
-export default App
+export default App;
